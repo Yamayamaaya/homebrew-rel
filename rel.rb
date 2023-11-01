bded113deecb0965238ed303fb49d8a68fd228df
Yamayamaaya <100800509+Yamayamaaya@users.noreply.github.com>
2023-11-01 09:47:44 +0900

    Update rel.rb

--- rel.rb
+++ rel.rb
@@ -2,7 +2,7 @@ class Rel < Formula
   desc "Description of the rel script"
   homepage "https://github.com/Yamayamaaya/Rel"
   url "https://github.com/Yamayamaaya/Rel/raw/main/src/rel.sh"
-  sha256 "your_script_sha256"
+  sha256 "07144df815d8ed3ecb59f570ee469a5e13c727100c94469d85454ba99e6fa424"
   version "1.0.0"
 
   def install
